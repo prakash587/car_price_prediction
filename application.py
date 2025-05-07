ba323fadd971435c67cfc2db23d517e0458b673c
prakash587 <80191742+prakash587@users.noreply.github.com>
2025-05-07 21:31:25 +0545

    Updated for Render deployment with Waitress

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import pandas as pd
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -47,4 +48,4 @@ def predict():
         return "Error"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
